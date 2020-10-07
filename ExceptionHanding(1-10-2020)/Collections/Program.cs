85c143596f2234c921b3aa0df5d4d38d1fa2821d
unknown <dilipkumardk4848@gmail.com>
2020-10-07 19:09:50 +0530

    exceptionhandling

---
+++ ExceptionHanding(1-10-2020)/Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Collections
+    {
+        public static void Generics()//generics
+        {
+            List<int> mylist = new List<int>();//creating an collection intializer for list
+            for(int numbers= 5;numbers<10;numbers++)
+            {
+                mylist.Add(numbers * 3);
+            }
+            foreach (int items in mylist)//for each is used to traverese the data
+                Console.WriteLine(items);
+        }
+        public static void NonGenerics()//non generics 
+        {
+            Queue myQueue = new Queue();
+            myQueue.Enqueue("FirstName");
+            myQueue.Enqueue("MiddleName");
+            myQueue.Enqueue("lastName");
+
+            Console.WriteLine("total number of name format available is ");
+            Console.WriteLine(myQueue.Count);
+            Console.WriteLine("begining item of myqueue is " + myQueue.Peek());
+                
+        }
+        static void Main(string[] args)
+        {
+            Generics();
+            NonGenerics();
+            
+        }
+    }
+}
---
+++ ExceptionHanding(1-10-2020)/ExceptionHanding/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ExceptionHanding
+{
+    class Exceptionhandling
+    {
+        public static void  HandlingAnError()
+        {
+            Console.Write("Please enter a number to divide 100: ");
+            
+
+            try
+            {
+                int num = int.Parse(Console.ReadLine());//returns string values
+
+                int result = 100 / num;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException )
+            {
+                Console.WriteLine("Cannot divide by zero. Please try again.");
+            }
+            catch (FormatException )
+            {
+                Console.WriteLine("Not a valid format. Please try again.");
+            }
+            
+        }
+        public  static void HandlingOtherError()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader("new.txt");//reading a file
+                {
+                    reader.ReadToEnd();// reading till the last character
+                }
+                
+            }
+            catch(Exception)
+            {
+                Console.WriteLine(" error occured");
+            }
+             finally
+            {
+
+                Console.WriteLine(" try searching the file present in database");
+            }
+        }
+         public static void checkingForAge(int Age)
+        { 
+            if(Age < 18)
+            {
+                throw new ArgumentException("you are not elligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("you are elligible for voting");
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            HandlingAnError();
+            HandlingOtherError();
+            checkingForAge(14);
+        }
+    }
+}
