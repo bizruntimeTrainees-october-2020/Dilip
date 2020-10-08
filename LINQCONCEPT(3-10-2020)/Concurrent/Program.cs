6c15b23555d24018a7f0334fc34ad593c6f4c15f
unknown <dilipkumardk4848@gmail.com>
2020-10-08 07:10:42 +0530

    Linq

---
+++ LINQCONCEPT(3-10-2020)/Concurrent/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Concurrent
+{
+    class Concurrents
+    {
+        public static void Concurrenttype()
+        {
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            int count = ints.Count;
+            Console.WriteLine(count);
+
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+
+            stack.Push(41);
+            stack.Push(04);
+            stack.Push(010);
+            stack.Push(101);
+            stack.Push(200);
+            stack.Push(004);
+
+            if (stack.IsEmpty)
+            {
+                Console.WriteLine("The stack is empty!");
+            }
+
+            else
+            {
+                Console.WriteLine("The stack isn't empty");
+            }
+            ArrayList list1 = new ArrayList();
+            list1.Add("One");
+            list1.Add("Two");
+            list1.Add("Three");
+            list1.Add("Four");
+            list1.Add("Five");
+            Console.WriteLine("Elements in ArrayList...");
+            foreach (string res in list1)
+            {
+                Console.WriteLine(res);
+            }
+            ArrayList list = ArrayList.Synchronized(list1);
+            Console.WriteLine("Is ArrayList synchronized? = " + list.IsSynchronized);
+
+            ConcurrentDictionary<string, string> dictionary = new ConcurrentDictionary<string, string>();
+            dictionary.TryAdd("1", "First");
+            dictionary.TryAdd("2", "Second");
+            dictionary.TryAdd("3", "Third");
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key + "-" + item.Value);
+            }
+            string removedItem;
+            bool result = dictionary.TryRemove("2", out removedItem); //Returns true
+            Console.WriteLine(removedItem);
+
+             
+        }
+         public static void Main(string[] args)
+        {
+            Concurrenttype();
+        }
+
+    }
+}
---
+++ LINQCONCEPT(3-10-2020)/LINQCONCEPT/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQCONCEPT
+{
+    class Linq
+    {
+
+        public static void SimpleLinq()//QUERY SYNTAX
+        {
+            List<int> List = new List<int> {1,2,3, 4, 5, 6, 7, 8, 9 };
+            var querySyntax = from obj in List
+                              where obj > 2
+                              select obj;
+             foreach ( var items in querySyntax)
+            {
+                Console.WriteLine(items);
+            }
+            Console.WriteLine("-------------------------");
+     
+            var methodsyntax = List.Where(obj => obj > 2);// method syntax and lambda expression
+            foreach (var items in methodsyntax)
+            {
+                Console.WriteLine(items);
+            }
+            Console.WriteLine("-------------------------");// mixed syntax
+            int[] scores = new int[] { 97, 92, 81, 60 };
+            int minimum = scores.Min();//  finding minimum value 
+            Console.WriteLine(minimum);
+            int sum1 = scores.Sum();// finding sum of the values 
+            Console.WriteLine(sum1);
+
+            var mixedSyntax = (from obj1 in scores
+                               select obj1).Max();// finding maximum values
+            
+            Console.WriteLine(mixedSyntax);
+            Console.WriteLine("-------------------------");// operations orderby
+            string[] words = { "apple", "pineapple","blueberry", "cheery"};
+            var sortedWords = from items in words orderby items select items;
+            foreach( var items in sortedWords)
+            {
+                Console.WriteLine(items);
+            }
+            
+        }
+        public static void enumerable()// enumerable 
+        {
+            int[] scores = new int[]{ 54, 59, 51, 50, 49, 45};
+            IEnumerable<int> scoreQuery = from score in scores where score > 50 select score;
+            foreach (var item in scores )
+            {
+                Console.WriteLine(item);
+            }
+        
+        }
+
+
+         public static void Main(string[] args)
+        {
+            SimpleLinq();
+            enumerable();
+        }
+    }
+}
