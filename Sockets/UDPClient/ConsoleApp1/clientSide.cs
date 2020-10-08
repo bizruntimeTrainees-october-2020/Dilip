4c2674b4f72b858507a043d4926d12311079bf25
unknown <dilipkumardk4848@gmail.com>
2020-10-08 18:55:51 +0530

    sockets

---
+++ Sockets/UDPClient/ConsoleApp1/clientSide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace client
+{
+    class ClientSid//protocoltype UDP
+    {
+        public static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+            Console.WriteLine(">");
+            string inputs = Console.ReadLine();
+            if(inputs!=null)
+            {
+                byte[] bytesent = Encoding.ASCII.GetBytes(inputs);
+                client.Send(bytesent, bytesent.Length);
+                Console.WriteLine("> successfull message sent");
+                client.Close();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
