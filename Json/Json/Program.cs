7605036dac972e7ed40f74f38607325048f39662
unknown <dilipkumardk4848@gmail.com>
2020-10-15 19:13:08 +0530

    newtonSoft Json.net

---
+++ Json/Json/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+    [Serializable]//serializa
+    class Training//class to be seialized
+    {
+        public int ID;
+        public String Name;
+        static void Main(string[] args)
+        {
+            Training obj = new Training();//intializing an object
+            obj.ID = 1001;
+            obj.Name = "Biz run time";
+
+            IFormatter formatter = new BinaryFormatter();//creating a file stream and convert object to binary file
+            Stream stream = new FileStream(@"G:\Example.txt", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, obj);//serialize object
+            stream.Close();
+
+            stream = new FileStream(@"G:\Example.txt", FileMode.Open, FileAccess.Read);//creating the file name 
+            Training objnew = (Training)formatter.Deserialize(stream);//deserialize the object 
+
+            Console.WriteLine(objnew.ID);//writing data to console
+            Console.WriteLine(objnew.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
