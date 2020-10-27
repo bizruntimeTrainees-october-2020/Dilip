208d8d27b9246e35cf9000521010a6a358283284
dilip kumar <dilipkumardk4848@gmail.com>
2020-10-27 12:15:27 +0530

    ASP.net

---
+++ ASPNET/FirstCodeOnASP/FirstCodeOnASP/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace FirstCodeOnASP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<IStartup>();
+            });
+    }
+}
+
---
+++ ASPNET/FirstCodeOnASP/FirstCodeOnASP/startupcs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCodeOnASP
+{
+    class startupcs
+    {
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+              endpoints.MapGet("/",  async context =>
+                {
+                    await context.Response.WriteAsync("hello from biz run time");
+
+                });
+            });
+        }
+
+        private Task asyc(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
